7012066288a06811228464a241ec3af2087290b0
Jakub Zurek <kzurek@gmail.com>
2019-06-16 18:56:06 +0200

    Initial project setup - Redux

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import { connect} from 'react-redux';
 
-export default class App extends Component {
+import Input from '../components/Input';
+import { setMessage } from '../actions/message';
+
+class App extends Component {
+    onChange = (value) => {
+        this.props.dispatch(setMessage(value));
+    }
+
     render () {
-        return <p>Kalkulator PIT/ZG</p>
+        const { message } = this.props.messageReducer;
+        return (
+            <Input value={message} onChange={this.onChange} />
+        );
     }
 }
+
+export default connect(state => state)(App);
