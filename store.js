18fffa17b04b68db70c3786f9ecff4bd405196a7
Jakub Zurek <kzurek@gmail.com>
2019-07-02 19:34:52 +0200

    Redux DevTools setup

--- store.js
+++ store.js
@@ -1,4 +1,9 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware,
+  compose,
+} from 'redux';
 import thunk from 'redux-thunk';
 import messageReducer from './reducers/message';
 
@@ -6,9 +11,14 @@ const reducer = combineReducers({
   messageReducer,
 });
 
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   reducer,
-  applyMiddleware(thunk),
+  composeEnhancers(
+    applyMiddleware(thunk),
+  ),
 );
 
 export default store;
